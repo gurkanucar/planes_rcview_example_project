2a54ac7f51324d15b510a3195d7eeda5cb91f495
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-19 20:23:01 +0300

    plane model created

---
+++ app/src/main/java/com/gusoft/planes_rcview_example_project/PlaneModel.kt
@@ -0,0 +1,11 @@
+package com.gusoft.planes_rcview_example_project
+
+data class PlaneModel(
+    val name: String,
+    val image: Int,
+    val country: String,
+    val cost: Int,
+    val speed: Int,
+    val range: Int,
+    val date: String,
+)
