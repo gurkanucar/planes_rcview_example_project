f605730e8b2373f1dd441d404765c19b1a7c5772
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-19 22:29:40 +0300

    item click functions implemented

--- app/src/main/java/com/gusoft/planes_rcview_example_project/HomeFragment.kt
+++ app/src/main/java/com/gusoft/planes_rcview_example_project/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.gusoft.planes_rcview_example_project.databinding.FragmentHomeBinding
 
@@ -36,7 +37,13 @@ class HomeFragment : Fragment() {
             planesRcView.adapter = planeAdapter
             planesRcView.setHasFixedSize(true)
         }
+
+        planeAdapter.onItemClick =::onItemClick
+        planeAdapter.onDeleteClick =::onDeleteClick
+        planeAdapter.onShowClick =::onShowClick
+
         binding.planesRcView.adapter = planeAdapter
+
     }
 
 
@@ -56,6 +63,27 @@ class HomeFragment : Fragment() {
 
         return list
     }
+
+
+    fun onItemClick(planeModel: PlaneModel){
+        Toast.makeText(requireContext(),
+            "Item Clicked -> $planeModel"
+            ,Toast.LENGTH_SHORT)
+            .show()
+    }
+    fun onDeleteClick(planeModel: PlaneModel){
+        Toast.makeText(requireContext(),
+            "Delete Clicked -> $planeModel"
+            ,Toast.LENGTH_SHORT)
+            .show()
+    }
+    fun onShowClick(planeModel: PlaneModel){
+        Toast.makeText(requireContext(),
+            "Show Clicked -> $planeModel"
+            ,Toast.LENGTH_SHORT)
+            .show()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/gusoft/planes_rcview_example_project/PlaneAdapter.kt
+++ app/src/main/java/com/gusoft/planes_rcview_example_project/PlaneAdapter.kt
@@ -11,6 +11,10 @@ import kotlin.collections.ArrayList
 class PlaneAdapter(private var data: ArrayList<PlaneModel>) :
     RecyclerView.Adapter<PlaneAdapter.ViewHolder>() {
 
+    var onItemClick: (PlaneModel) -> Unit = {}
+    var onDeleteClick: (PlaneModel) -> Unit = {}
+    var onShowClick: (PlaneModel) -> Unit = {}
+
     class ViewHolder(val binding: RcViewItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -25,6 +29,9 @@ class PlaneAdapter(private var data: ArrayList<PlaneModel>) :
         val numberFormat = NumberFormat.getCurrencyInstance(Locale("en", "US"))
 
         holder.binding.apply {
+            planeCard.setOnClickListener { onItemClick(planeModel) }
+            showButton.setOnClickListener { onShowClick(planeModel) }
+            deleteButton.setOnClickListener { onDeleteClick(planeModel) }
             planeName.text = planeModel.name
             planeImage.setImageResource(planeModel.image)
             country.text = "max speed: ${planeModel.country} mph"
