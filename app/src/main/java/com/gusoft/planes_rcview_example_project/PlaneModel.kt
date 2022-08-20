d617f7eadc36946cc796b21b1686ec8d866d9ae0
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-20 09:11:40 +0300

    parcelize plugin added

--- app/src/main/java/com/gusoft/planes_rcview_example_project/PlaneModel.kt
+++ app/src/main/java/com/gusoft/planes_rcview_example_project/PlaneModel.kt
@@ -1,8 +1,10 @@
 package com.gusoft.planes_rcview_example_project
 
+import kotlinx.parcelize.Parcelize
 import android.os.Parcel
 import android.os.Parcelable
 
+@Parcelize
 data class PlaneModel(
     val name: String?,
     val image: Int,
@@ -11,39 +13,4 @@ data class PlaneModel(
     val speed: Int,
     val range: Int,
     val date: String?,
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readDouble(),
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.readString()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeInt(image)
-        parcel.writeString(country)
-        parcel.writeDouble(cost)
-        parcel.writeInt(speed)
-        parcel.writeInt(range)
-        parcel.writeString(date)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<PlaneModel> {
-        override fun createFromParcel(parcel: Parcel): PlaneModel {
-            return PlaneModel(parcel)
-        }
-
-        override fun newArray(size: Int): Array<PlaneModel?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
